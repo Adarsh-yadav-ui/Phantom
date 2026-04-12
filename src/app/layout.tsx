42b02b227b141acd922601046f79e0c07dffc0c8
Adarsh Yadav <akankshaazm20@gmail.com>
2026-04-12 01:36:24 -0700

    06: Codebase Reincarnation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
       lang="en"
       className={`${josefinSans.className} ${josefinSans.style} h-full antialiased`}
     >
+
       <body className="min-h-full flex flex-col">
         <ClerkProvider>
           <ConvexClientProvider>{children}</ConvexClientProvider>
